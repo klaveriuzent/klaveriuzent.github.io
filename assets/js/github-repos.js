83792877be091936c820caf083fa7f619136bf24
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-04-26 08:59:18 +0700

    add fork label

--- assets/js/github-repos.js
+++ assets/js/github-repos.js
@@ -13,26 +13,31 @@ fetch("https://api.github.com/users/klaveriuzent/repos?sort=pushed&per_page=6")
           const article = document.createElement("article");
           article.className = "col-6 col-12-xsmall work-item";
           article.innerHTML = `
-    <div class="card">
-      <h3 class="repo-name">${repo.name}</h3>
-      <p>
-          License: ${
-            repo.license
-              ? '<img class="license-icon" style="opacity: 0.45; vertical-align: middle; margin-top: -2px" src="/assets/icon/scale_balance_icon.svg" alt="License icon">' +
-                repo.license.name
-              : "No license"
-          }
-      </p>
-      <p>Language: ${Object.keys(languages).join(", ")}</p>
-      <p class="repo-atr">Description :</p>
-      <p class="repo-description">${
-        repo.description ? repo.description : "No description"
-      }</p>
-      <div class="card-link">
-        <a href="${repo.html_url}" class="button-mod">View on GitHub</a>
-      </div>
-    </div>
-  `;
+            <div class="card">
+              <div class="card-header">
+                <h3 class="repo-name">${repo.name}</h3>
+                ${repo.fork ? '<span class="fork-label">Fork</span>' : ''}
+              </div>
+              <div class="card-body">
+                <p>
+                  License: ${
+                    repo.license
+                      ? '<img class="license-icon" style="opacity: 0.45; vertical-align: middle; margin-top: -2px" src="/assets/icon/scale_balance_icon.svg" alt="License icon">' +
+                        repo.license.name
+                      : "No license"
+                  }
+                </p>
+                <p>Language: ${Object.keys(languages).join(", ")}</p>
+                <p class="repo-atr">Description :</p>
+                <p class="repo-description">${
+                  repo.description ? repo.description : "No description"
+                }</p>
+                <div class="card-link">
+                  <a href="${repo.html_url}" class="button-mod">View on GitHub</a>
+                </div>
+              </div>
+            </div>
+          `;
           repoList.appendChild(article);
         })
         .catch((error) => console.error(error));
